ff8e07e3ea733449a6a444d9cccfacbad985b95b
Immanuel Hardjo <31676311+KutuIjo@users.noreply.github.com>
2019-12-04 21:39:07 +0700

    Update training_image_classifier.py

--- training_image_classifier.py
+++ training_image_classifier.py
@@ -11,7 +11,7 @@ img_width, img_height = 150, 150
 train_data_dir = 'data/train'
 validation_data_dir = 'data/validation'
 nb_train_samples = 1000
-nb_validation_samples = 800
+nb_validation_samples = 199
 epochs = 50
 batch_size = 16
 
